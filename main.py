0e0f7bf7a377ac9205ac4b7ac32fbfbd2f10c651
hakaboom <43911741+hakaboom@users.noreply.github.com>
2021-08-08 01:05:46 +0800

    性能监控:增加多个内存获取接口

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ python setup.py sdist
 twine upload dist/*
 """
 import re
+import time
+from loguru import logger
 
 from adbutils import ADBExtraDevice
+from adbutils.extra.performance import Performance
 
 device = ADBExtraDevice(device_id='emulator-5554', aapt=True)
-# print(device.shell(['busybox']))
-meminfo = device.shell(['dumpsys', 'meminfo'])
-# print(meminfo)
-pattern = re.compile(r'Total PSS by process:\r\n   (.*)\r\n   ', re.DOTALL)
-print(pattern.findall(meminfo))
-# pattern = re.compile(r'\s(\d+) kB: (\S+)\s*\(pid (\d+).*\)')
-# if m := pattern.findall(meminfo):
-#     for i in m:
-#         print(i)
+performance = Performance(device)
+package_name = 'jp.co.cygames.umamusume'
+
+
+# sys_memory = performance.get_system_meminfo()
+# ret = performance.get_pss_by_process(sys_memory)
+# total_memory = performance.get_total_ram(sys_memory)
+# lost_memory = performance.get_lost_ram(sys_memory)
+# using_memory = 0
+# for i in ret:
+#     using_memory += i[0]
+#
+# print(total_memory - using_memory, using_memory)
+# print(f'{((total_memory - using_memory - lost_memory) / 1024):.0f}MB')
+total = performance.get_app_meminfo(package_name)['total'][0]
+print(f'{(total / 1024):.0f}MB')
