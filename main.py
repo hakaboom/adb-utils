6336b2c4a34a52dfb462f84ff1d9d5da8292a08b
hakaboom <43911741+hakaboom@users.noreply.github.com>
2021-08-08 23:12:07 +0800

    init

--- main.py
+++ main.py
@@ -10,11 +10,4 @@ from adbutils import ADBExtraDevice
 from adbutils.extra.performance.meminfo import Meminfo
 
 device = ADBExtraDevice(device_id='emulator-5554', aapt=True)
-performance = Meminfo(device)
-package_name = 'jp.co.cygames.umamusume'
-#
-
-
-for i in range(100):
-    total = performance.get_app_summary(package_name)['total']
-    print(f'{(int(total)/ 1024):.0f}MB')
+meminf_watcher = Meminfo(device)
