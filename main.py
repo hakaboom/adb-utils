1f87cb499d193c5d86999d8556239ed217014e25
hakaboom <70704881@qq.com>
2021-08-28 22:06:36 +0800

    update setup.py

--- main.py
+++ main.py
@@ -11,9 +11,3 @@ from loguru import logger
 from adbutils import ADBDevice, ADBClient
 from adbutils.extra.performance.fps import Fps
 
-
-device = ADBDevice('emulator-5554')
-fps = Fps(device)
-# layers = fps.get_possible_layer(device.foreground_package)
-print(fps.get_layers_from_buffering())
-
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='adb-utils',
-    version='1.0.6',
+    version='1.0.7',
     author='hakaboom',
     author_email='1534225986@qq.com',
     license='Apache License 2.0',
