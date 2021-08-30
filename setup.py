5165db2f570df9553c7d13b6631cbb39a97228c0
huangziqing <270704881@qq.com>
2021-08-30 13:34:03 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='adb-utils',
-    version='1.0.8',
+    version='1.0.12',
     author='hakaboom',
     author_email='1534225986@qq.com',
     license='Apache License 2.0',
     description='This is a secondary package of adb',
     url='https://github.com/hakaboom/base_image',
-    packages=['adbutils'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=["baseImage==1.0.6"],
 )
